c2b08ca055e2b41da546400b286eec81ab95ff8d
Shalqarov <Temirlan200312@gmail.com>
2022-05-05 00:57:59 +0600

    added: hub struct

--- delivery/web/websocket_handler.go
+++ delivery/web/websocket_handler.go
@@ -1,6 +1,32 @@
 package web
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type client struct {
+	hub  *Hub
+	conn *websocket.Conn
+	send chan []byte
+}
+
+type Hub struct {
+	clients    map[*client]bool
+	comments   chan []byte
+	register   chan *client
+	unregister chan *client
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		comments:   make(chan []byte),
+		register:   make(chan *client),
+		unregister: make(chan *client),
+		clients:    make(map[*client]bool),
+	}
+}
 
 func webSocket(w http.ResponseWriter, r *http.Request) {
 	// TODO serve ws
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Shalqarov/real-time-comments
 
 go 1.17
+
+require github.com/gorilla/websocket v1.5.0 // indirect
